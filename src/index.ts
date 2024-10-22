46cd34cdaf13156d02fa30878d2ca2ede3f605af
JonashGana <jonashtiglao@gmail.com>
2024-10-22 22:39:19 +0800

    fix initializing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import './style.css';
 import { fetchAllCountries } from './api/fetchCountries';
+import RenderAllCountries, { pageState } from './components/RenderAllCountries';
+import { handlePagination, filterCountriesByRegion } from './components/eventHandlers';
+import { setIsFilteredByRegion, setCurrentCountries, getCurrentCountries } from './helpers/util';
 
-window.onload = fetchAllCountries;
+async function initialize() {
+  const countries = await fetchAllCountries();
+
+  if (countries) {
+    // setIsFilteredByRegion(false);
+    setCurrentCountries(countries);
+    pageState.currentPage = 1;
+
+    handlePagination(getCurrentCountries());
+    RenderAllCountries(getCurrentCountries());
+
+    filterCountriesByRegion();
+  }
+}
+
+window.onload = initialize;
