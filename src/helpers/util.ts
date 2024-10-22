8fc13b4fc84e1d42a28afd809f8a0447e2f1e19a
JonashGana <jonashtiglao@gmail.com>
2024-10-22 22:38:56 +0800

    add other utility functions

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,3 +1,30 @@
+interface Country {
+  name: { common: string };
+  capital: string;
+  region: string;
+  population: number;
+  flags: { svg: string };
+}
+
+let isFilteredByRegion = false;
+let currentCountries: Country[] = [];
+
+export const setIsFilteredByRegion = (value: boolean) => {
+  isFilteredByRegion = value;
+};
+
+export const getIsFilteredByRegion = () => {
+  return isFilteredByRegion;
+};
+
+export const setCurrentCountries = (countries: Country[]) => {
+  currentCountries = countries;
+};
+
+export const getCurrentCountries = () => {
+  return currentCountries;
+};
+
 export const randomizeArray = (arr: []) => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
