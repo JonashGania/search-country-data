b808b9002b9bcf2cbfbf4c61ce4e77593a445fb7
JonashGana <jonashtiglao@gmail.com>
2024-10-22 22:37:33 +0800

    added fetch countries by region function

--- src/api/fetchCountries.ts
+++ src/api/fetchCountries.ts
@@ -1,15 +1,41 @@
-import RenderAllCountries from '../components/RenderAllCountries';
 import { randomizeArray } from '../helpers/util';
 
-export async function fetchAllCountries() {
+interface Countries {
+  name: { common: string };
+  capital: string;
+  region: string;
+  population: number;
+  flags: { svg: string };
+}
+
+export async function fetchAllCountries(): Promise<Countries[] | undefined> {
   let countries = [];
 
   try {
     const response = await fetch('https://restcountries.com/v3.1/all');
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.status}`);
+    }
+
     const result = await response.json();
     const randomizeCountries = randomizeArray(result);
     countries = randomizeCountries;
-    RenderAllCountries(countries);
+
+    return countries;
+  } catch (error) {
+    console.error('Failed to fetch countries', error);
+  }
+}
+
+export async function fetchCountriesByRegion(region: string): Promise<Countries[] | undefined> {
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.status}`);
+    }
+    const result = await response.json();
+
+    return result;
   } catch (error) {
     console.error('Failed to fetch countries', error);
   }
