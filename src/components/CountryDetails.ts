bf5550be4195bd7d063d93c0c04d149f019c9914
JonashGana <jonashtiglao@gmail.com>
2024-10-26 21:57:37 +0800

    lint fix

--- src/components/CountryDetails.ts
+++ src/components/CountryDetails.ts
@@ -35,12 +35,13 @@ const CountryDetails = (countryDetails: CountryDetails) => {
   countryContentDetails?.appendChild(CreateCountyDetails(countryDetails));
 };
 
-async function initMap (countryCode: string, mapContainer: HTMLDivElement) {
-  const map = L.map(mapContainer).setView([0, 0], 13);
+async function initMap(countryCode: string, mapContainer: HTMLDivElement) {
+  const map = L.map(mapContainer, { scrollWheelZoom: false }).setView([0, 0], 13);
 
   L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   }).addTo(map);
 
   try {
@@ -64,6 +65,17 @@ async function initMap (countryCode: string, mapContainer: HTMLDivElement) {
     console.error('Error loading geoJSON file', error);
   }
 
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Control') {
+      map.scrollWheelZoom.enable();
+    }
+  });
+
+  window.addEventListener('keyup', (event) => {
+    if (event.key === 'Control') {
+      map.scrollWheelZoom.disable();
+    }
+  });
 }
 
 export default CountryDetails;
