301db62e485f502fa33267713bb5f7870e1387ea
JonashGana <jonashtiglao@gmail.com>
2024-10-26 21:57:18 +0800

    added searchCountryDetails

--- src/api/fetchCountries.ts
+++ src/api/fetchCountries.ts
@@ -1,5 +1,7 @@
-import { randomizeArray } from '../helpers/util';
-import { Country, CountryDetails } from '../interface/countryInterface';
+import { randomizeArray, applyDetailsPageSkeleton } from '../helpers/util';
+import { Country, CountryDetails as CountryDetailsInterface } from '../interface/countryInterface';
+import { DisplayNoResult } from '../components/DisplayNoResult';
+import CountryDetails from '../components/CountryDetails';
 
 export async function fetchAllCountries(): Promise<Country[] | undefined> {
   let countries = [];
@@ -34,11 +36,11 @@ export async function fetchCountriesByRegion(region: string): Promise<Country[] 
   }
 }
 
-export async function fetchCountriesDetails(countryName: string): Promise<CountryDetails | undefined> {
+export async function fetchCountriesDetails(
+  countryCode: string,
+): Promise<CountryDetailsInterface | undefined> {
   try {
-    const formattedEndpoint = countryName.split(' ').join('%20');
-
-    const response = await fetch(`https://restcountries.com/v3.1/name/${formattedEndpoint}`);
+    const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
     if (!response.ok) {
       throw new Error(`API Error: ${response.status}`);
     }
@@ -46,5 +48,47 @@ export async function fetchCountriesDetails(countryName: string): Promise<Countr
     return result[0];
   } catch (error) {
     console.error('Failed to fetch countries', error);
+  }
+}
+
+export async function searchCountryDetails() {
+  const searchInput = <HTMLInputElement>document.getElementById('search-country-input');
+  const searchInputValue = searchInput.value.trim();
+  const home = document.querySelector('.home') as HTMLElement;
+  const countryDetails = document.querySelector('.country-details') as HTMLElement;
+
+  home.classList.add('hidden');
+  countryDetails?.classList.remove('hidden');
+
+  countryDetails.innerHTML = '';
+
+  applyDetailsPageSkeleton();
+
+  if (!searchInputValue) {
+    DisplayNoResult();
+    return;
+  }
+
+  try {
+    const formattedEndpoint = searchInputValue.split(' ').join('%20');
+
+    const response = await fetch(`https://restcountries.com/v3.1/name/${formattedEndpoint}`);
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    if (result) {
+      setTimeout(() => {
+        CountryDetails(result[0]);
+      }, 1000);
+    }
+
+    searchInput.value = '';
+  } catch (error) {
+    DisplayNoResult();
+    searchInput.value = '';
+    console.error(error);
   }
 }
